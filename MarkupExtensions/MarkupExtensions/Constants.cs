5376f32d52ae40d14e9225407ba82e7ff7787330
Aluno <liviarsouza@gmail.com>
2018-10-19 20:24:57 -0300

    markup extensions

---
+++ MarkupExtensions/MarkupExtensions/Constants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace MarkupExtensions
+{
+    public class Constants
+    {
+        public static Color backgroundColor = Color.Yellow;
+        public static LayoutOptions centerAlignment = LayoutOptions.CenterAndExpand;
+        public static FontAttributes fontBold = FontAttributes.Bold;
+    }
+}
---
+++ MarkupExtensions/MarkupExtensions/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MarkupExtensions
+{
+    public partial class MainPage : ContentPage
+    {
+        public static string defaultText = "Default static text";
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            Navigation.PushAsync(new SecondPage());
+        }
+    }
+}
