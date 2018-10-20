89647e2b5305b5c21fd32b9220ce5381ecd38e0e
Aluno <liviarsouza@gmail.com>
2018-10-19 21:38:51 -0300

    sap directives

---
+++ SAP/SAP.Android/obj/Debug/android/src/md5c31b0d7b97e48f483940f3282d9b5f1f/MainActivity.java
@@ -0,0 +1,47 @@
+package md5c31b0d7b97e48f483940f3282d9b5f1f;
+
+
+public class MainActivity
+	extends md51558244f76c53b6aeda52c8a337f2c37.FormsAppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("SAP.Droid.MainActivity, SAP.Android", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("SAP.Droid.MainActivity, SAP.Android", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
---
+++ SAP/SAP/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+#if __IOS__
+using UIKit;
+#elif __ANDROID_1__
+using Android.OS;
+#endif
+
+
+namespace SAP
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+#if __IOS__
+            UIDevice device = new UIDevice();
+            lblInfo.Text = String.Format("Model={0}, System={1}, Version={2}",
+                                         device.Model, device.SystemName, device.SystemVersion);
+
+
+#elif __ANDROID_1__
+             lblInfo.Text = String.Format("Model={0}, System={1}, Version={2}",
+                                          Build.Manufacturer, Build.Model, Build.VERSION.Release.ToString());
+
+#endif
+        }
+    }
+}
