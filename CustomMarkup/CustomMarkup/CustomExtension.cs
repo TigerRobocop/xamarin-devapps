e17f0e2c1897d1479cf797dbaeb0f5ec0fd997b9
Aluno <liviarsouza@gmail.com>
2018-10-19 21:24:06 -0300

    custom from markup lib

---
+++ CustomMarkup/CustomMarkup/CustomExtension.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CustomMarkup
+{
+    public class CustomExtension : IMarkupExtension
+    {
+        public int R {
+            get; set;
+        }
+
+        public int G
+        {
+            get; set;
+        }
+
+        public int B
+        {
+            get; set;
+        }
+
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return Color.FromRgb(R, G, B);
+        }
+    }
+}
---
+++ CustomMarkup/MarkupLib/ColorRGBExtension.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MarkupLib
+{
+    public class ColorRGBExtension : IMarkupExtension
+    {
+        public int R
+        {
+            get; set;
+        }
+
+        public int G
+        {
+            get; set;
+        }
+
+        public int B
+        {
+            get; set;
+        }
+
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return Color.FromRgb(R, G, B);
+        }
+    }
+}
