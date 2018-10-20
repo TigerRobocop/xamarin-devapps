2288de8d647f775a59ca24c77033a79638141877
Aluno <liviarsouza@gmail.com>
2018-10-20 12:05:45 -0300

    master detail navigation

---
+++ MasterDetail/MasterDetail/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MasterDetail
+{
+    public partial class MainPage : MasterDetailPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            pgMaster.BtnAdd.Clicked += Menu_Clicked;
+            pgMaster.BtnAbout.Clicked += Menu_Clicked;
+            pgMaster.BtnList.Clicked += Menu_Clicked;
+            pgMaster.BtnContact.Clicked += Menu_Clicked;
+        }
+
+        
+        void Menu_Clicked(object sender, EventArgs e)
+        {
+            Button bt = (Button)sender;
+            switch (bt.StyleId) {
+                case "add":
+                    this.Detail = new NavigationPage(new Add());
+                    break;
+                case "about":
+                    this.Detail = new NavigationPage(new About());
+                    break;
+                case "list":
+                    this.Detail = new NavigationPage(new List());
+                    break;
+                case "contact":
+                    this.Detail = new NavigationPage(new Contact());
+                    break;
+            }
+
+            this.IsPresented = false;
+        }
+
+    }
+}
---
+++ MasterDetail/MasterDetail/Master.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace MasterDetail
+{
+    public partial class Master : ContentPage
+    {
+        public Master()
+        {
+            InitializeComponent();
+        }
+
+
+
+        public Button BtnAdd => btnAdd;
+
+
+        public Button BtnAbout => btnAbout;
+
+        public Button BtnList => btnList;
+
+        public Button BtnContact => btnContact;
+
+
+
+    }
+}
