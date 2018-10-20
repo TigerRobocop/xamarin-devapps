3acc850fd09e413af54b91a30a06c835639b6b36
Aluno <liviarsouza@gmail.com>
2018-10-20 15:27:44 -0300

    web beowser

---
+++ BrowserBinding/BrowserBinding/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace BrowserBinding
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void Handle_Completed(object sender, System.EventArgs e)
+        {
+            webBrowser.Source = inputUrl.Text;
+        }
+
+        void btnBack_Clicked(object sender, System.EventArgs e)
+        {
+            webBrowser.GoBack();
+        }
+
+        void btnGo_Clicked(object sender, System.EventArgs e)
+        {
+            webBrowser.GoForward();
+        }
+    }
+}
