4d6a73fc80867ad07e2ff8ba1719f1ed4d6f9fd7
Aluno <liviarsouza@gmail.com>
2018-10-20 14:47:59 -0300

    data binding

---
+++ Binding/Binding/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Binding
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Label newLabel = new Label();
+            newLabel.TextColor = Color.Red;
+
+
+            newLabel.BindingContext = slider;
+            newLabel.SetBinding(Label.TextProperty, "Value");
+
+
+            BoxView box = new BoxView();
+            box.Color = Color.Blue;
+            box.SetBinding(BoxView.OpacityProperty, new Binding
+            {
+                ImageSource = slider,
+                Path = "Value"
+            });
+
+            stack.Children.Add(newLabel);
+        }
+    }
+}
