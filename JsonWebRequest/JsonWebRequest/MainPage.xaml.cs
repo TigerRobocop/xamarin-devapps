ed6fc09726638ac158766dedb30216d5e2e08e75
Aluno <liviarsouza@gmail.com>
2018-10-20 16:26:56 -0300

    listview webrequest

---
+++ JsonWebRequest/JsonWebRequest/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using System.Net;
+using System.Runtime.Serialization.Json;
+using System.IO;
+
+namespace JsonWebRequest
+{
+    public partial class MainPage : ContentPage
+    {
+        WebRequest request;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            Uri uri = new Uri("https://developer.xamarin.com/demo/stock.json");
+            request = WebRequest.Create(uri);
+
+            lblLoading.Text = "lblLoading...";
+
+            request.BeginGetResponse(HandleAsyncCallback, null);
+
+
+        }
+
+        void HandleAsyncCallback(IAsyncResult ar)
+        {
+            Device.BeginInvokeOnMainThread(() =>
+            {
+
+
+                Stream stream = request.EndGetResponse(ar).GetResponseStream();
+                DataContractJsonSerializer serializer =
+                    new DataContractJsonSerializer(typeof(PhotoList));
+
+                PhotoList list = (PhotoList)serializer.ReadObject(stream);
+                listPhotos.ItemsSource = list.photos;
+
+                lblLoading.Text = "";
+            });
+
+
+        }
+
+    }
+}
---
+++ JsonWebRequest/JsonWebRequest/PhotoList.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace JsonWebRequest
+{
+    [DataContract]
+    public class PhotoList
+    {
+        [DataMember(Name = "photos")]
+        public List<string> photos;
+    }
+}
